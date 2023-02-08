5b95fcb2642b6f1e8d89beeff34a7bd42b298ce0
Ana Djurovic <anadjurovic129@gmail.com>
2023-02-08 18:39:06 +0100

    Optimization #2

--- app.js
+++ app.js
@@ -32,3 +32,23 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
   modal_container.classList.remove("show");
 });
+
+// Scroll reveal left
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("load", reveal);
